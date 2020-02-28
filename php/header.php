6af5597f1609a59209381e0b3f46b97f377e1ef5
jvansandt <jvansandt6688@gmail.com>
2020-02-27 21:04:16 -0800

    php test

--- php/header.php
+++ php/header.php
@@ -1,32 +1,9 @@
 <?php 
 
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>php PNW X-ray</title>
+<h1>Testing php link.</h1>
+    <h1>Testing php link.</h1>
+    <h1>Testing php link.</h1>
+    <h1>Testing php link.</h1>
     
-<link href="css/styles.css" type="text/css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
-</head>
-    
-    <body>
-        
-        <header id="top">
-            <div class="wrap">
-            <img src="images/logowhite.png">
-            <p>X-ray related accessories,<br> parts, supplies, and equipment</p>
-            </div>
-        </header>
-        
-        <nav class="wrap">
-            <ul>
-                <li><a href="store.html">Store</a></li>
-                <li><a href="contact.html">Contact Us</a></li>
-                <li><a href="faq.html">FAQ</a></li>
-                <li><a href="links.html">Links</a></li>
-            </ul>
-        </nav>
     
     ?>
